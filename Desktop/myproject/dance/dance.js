c61f23b04bfac0f17c9e1ee65f910d4304b5203e
Tiesha Sims <pkeogh1024@yahoo.com>
2016-12-12 17:01:29 -0700

    fixed coding error

--- Desktop/myproject/dance/dance.js
+++ Desktop/myproject/dance/dance.js
@@ -1,8 +1,19 @@
 for (var i = 0; i < 2; i++) {
   pointLeft();
   pointRight();
+  stepLeft();
+  stepRight();
 }
-stepLeft();
-stepRight();
-stepLeft();
-stepRight();
+
+function pointLeft(){
+  console.log("pointLeft")
+}
+function pointRight(){
+  console.log("pointRight");
+}
+function stepLeft(){
+  console.log("stepLeft");
+}
+function stepRight() {
+  console.log("stepRight");
+}
